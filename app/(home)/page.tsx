8f8807c07a3f714cf82b9d478a61d7d516482bcb
Marcos <marcoscaru@hotmail.com>
2024-02-07 15:30:11 -0300

    feat: add welcome messsage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,26 @@
 import Image from "next/image";
 import Header from "../_components/header";
+import {format} from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
 
   return <div>
 
        <Header />
+
+       <h2>Olá, Miguel!</h2>
+       <p className="capitalize">{format(new Date(), "EEEE',' dd 'de' MMMM", {
+         locale: ptBR 
+         })}
+         </p>
+
+
+
+
+       
+
+       
 
   </div>
  
