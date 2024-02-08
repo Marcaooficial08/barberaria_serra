29727f51e4ad7f7fe0255415b5f1945d9099780f
Marcos <marcoscaru@hotmail.com>
2024-02-08 11:21:00 -0300

    fead: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,26 +2,33 @@ import Image from "next/image";
 import Header from "../_components/header";
 import {format} from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { Search } from "lucide-react";
+import OTAVIO from "./_components/search";
+import Final from "./_components/search";
+
+
 
 export default function Home() {
 
-  return <div>
+  return ( 
+  <div>
 
        <Header />
-
-       <h2>Olá, Miguel!</h2>
-       <p className="capitalize">{format(new Date(), "EEEE',' dd 'de' MMMM", {
-         locale: ptBR 
+         
+        <div className="px-5 pt-5">
+       <h2 className="text-xl font-bold">Olá, Marcos!</h2>
+       <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {
+         locale: ptBR,
          })}
          </p>
+                
+         </div> 
+   
+          <div className="px-5 mt-6" >
+        <Final/>
+        </div>
 
-
-
-
-       
-
-       
-
+         
   </div>
- 
+  );
 }
