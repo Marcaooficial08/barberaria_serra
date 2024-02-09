346c6dbe2d8a82cbfc134cdbc4c54d46529c8cac
Marcos <marcoscaru@hotmail.com>
2024-02-09 10:53:56 -0300

    feat: add footer componets

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Header from "../_components/header";
-import {format} from "date-fns";
+import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { Search } from "lucide-react";
 import OTAVIO from "./_components/search";
@@ -9,55 +9,58 @@ import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
 
-
-
 export default async function Home() {
-  const barbershops = await db.barbershop.findMany({})
+  const barbershops = await db.barbershop.findMany({});
 
-  return ( 
-  <div>
+  return (
+    <div>
+      <Header />
 
-       <Header />
-         
-        <div className="px-5 pt-5">
-       <h2 className="text-xl font-bold">Olá, Marco!</h2>
-       <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {
-         locale: ptBR,
-         })}
-         </p>
-                
-         </div> 
-   
-          <div className="px-5 mt-6" >
-        <Final/>
-        </div>
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Marco!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
 
-        <div className="px-5 mt-6">
-          
-         <h2 className="text-sm mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
-          <BookingItem/>
-           </div>
+      <div className="px-5 mt-6">
+        <Final />
+      </div>
 
-          <div className="mt-6">
-          <h2 className="text-sm mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+      <div className="px-5 mt-6">
+        <h2 className="text-sm mb-3 uppercase text-gray-400 font-bold">
+          Agendamentos
+        </h2>
+        <BookingItem />
+      </div>
 
-          <div>
-  <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-    {barbershops.map((barbershop) => (
-      <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
-    ))}
-  </div>
-</div>
-
-           
-           
-          
+      <div className="mt-6">
+        <h2 className="text-sm mb-3 uppercase text-gray-400 font-bold">
+          Recomendados
+        </h2>
+        <div>
+          <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+            {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
           </div>
+        </div>
+      </div>
 
-
-       
-
-         
-  </div>
+      <div className="mt-6">
+        <h2 className="text-sm mb-3 uppercase text-gray-400 font-bold">
+          Populares
+        </h2>
+        <div>
+          <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+            {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
