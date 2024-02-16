17e7798babbffbd3f42919ee3cac84609f358a4e
Marcos <marcoscaru@hotmail.com>
2024-02-16 14:40:06 -0300

    feat: add next auth setup

--- app/barbershop/[id]/page.tsx
+++ app/barbershop/[id]/page.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from 'next/image';
+import BarbershopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
 
 
 interface BarbershopDetailsPageProps{
@@ -33,7 +35,10 @@ interface BarbershopDetailsPageProps{
         where:{
 
             id:params.id,
-        },
+        },include:
+        {
+          services: true,
+        }
          
     });
       if (!barbershop)
@@ -47,49 +52,18 @@ interface BarbershopDetailsPageProps{
 
 
         return (  
-               <div>
+          <div>
+               
+           <BarbershopInfo barbershop={barbershop}/>
+           <div className="px-5 flex flex-col gap-4 py-6">
+           { barbershop.services.map(service => (
+            
+            <ServiceItem key={service.id} service={service}/>
+            
 
-                <div className="h-[250px] w-full relative">
-                     
-
-
-                    <Button size="icon" variant="outline" className="z-50 absolute top-4 left-4">
-                      <ChevronLeftIcon/>
-
-                    </Button>
-
-                    <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
-                      <MenuIcon/>
-
-                    </Button>
-                 
-                    <Image src={barbershop.imageUrl} fill alt={barbershop.name} style={{
-                        objectFit: "cover",
-
-                    }}
-                    className="opacity-75"
-                    
-                    />
-
-                  
-
-                </div>
-                <div className="px-5  py-3 pb-6">
-
-              <h1 className="text-xl font-bold  ">{barbershop.name}</h1>
-             
-                 <div className="flex items-center gap-1 mt-2">
-                    <MapPinIcon className="text-primary" size={18}/>
-                 <p className="text-sm">{barbershop.address}</p>
-                 </div>
-                 <div className="flex items-center gap-1 mt-2">
-                    <StarIcon className="text-primary" size={18}/>
-                 <p className="text-sm">5,0 (1254 avaliações )</p>
-                 </div>
-              </div>
-
-               </div>
-           
+          )) }
+          </div>
+           </div>
         );
     }
      
