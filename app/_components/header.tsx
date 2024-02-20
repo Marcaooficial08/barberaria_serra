2a695c9de296910df9c32b849c433b38dba10f5c
Marcos <marcoscaru@hotmail.com>
2024-02-20 15:03:56 -0300

    feat: add SIdeMenu

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,53 +1,34 @@
 "use client";
-
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
-import { Button } from "./ui/button"
-import { MenuIcon } from "lucide-react";
-
-import { signIn, useSession } from 'next-auth/react'
+import { Button } from "./ui/button";
+import { CalendarIcon, HomeIcon, Link, LogOutIcon, MenuIcon, UserIcon } from "lucide-react";
+import { signIn, signOut, useSession } from 'next-auth/react';
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
 import { Avatar, AvatarImage } from "./ui/avatar";
+import SideMenu from "./side-menu";
 
-
-const  Header = () => {
+const Header = () => {
  
-  
-  
-    return ( 
 
-       <Card>
-        <CardContent className="p-5  justify-between items-center flex flex-row">
-
+  return (
+    <Card>
+      <CardContent className="p-5 justify-between items-center flex flex-row">
         <Image src="/Logo.png" alt="Logo" height={22} width={120} style={{ maxWidth: '100%', height: 'auto' }} />
         <Sheet>
-        <SheetTrigger asChild >
-          <Button variant="outline" size="icon" >
-           <MenuIcon size={16}/>
-           
-  </Button> 
-    </SheetTrigger>
-       <SheetContent className="p-0">
-          <SheetHeader className="text-left border-b border-secondary p-5">
-            <SheetTitle>
-              Menu
-            </SheetTitle>
-          </SheetHeader>
-         
-       </SheetContent>
-
-    </Sheet> 
-  
-                
-                              
-         </CardContent>
-
-       </Card>
-
-
-     );
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="p-0">
+            <SideMenu/>
+            
+          </SheetContent>
+        </Sheet>
+      </CardContent>
+    </Card>
+  );
 };
 
-
- 
 export default Header;
