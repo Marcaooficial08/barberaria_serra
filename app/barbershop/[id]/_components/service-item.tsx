b21601e76ab27f8a881691bab67d5abaef037062
Marcos <marcoscaru@hotmail.com>
2024-02-24 14:24:27 -0300

    feat: add service inital booking

--- app/barbershop/[id]/_components/service-item.tsx
+++ app/barbershop/[id]/_components/service-item.tsx
@@ -1,20 +1,47 @@
 "use client";
+
+
 import { Button } from "@/app/_components/ui/button";
+import { Calendar } from "@/app/_components/ui/calendar";
 import { Card, CardContent } from "@/app/_components/ui/card";
-import { Service } from "@prisma/client";
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/app/_components/ui/sheet";
+import { Barbershop, Service } from "@prisma/client";
+import { ptBR } from "date-fns/locale";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
-import { useRouter } from "next/router";
+
+import { useMemo, useState } from "react";
+import { generateDayTimeList } from "../_helpers/hours";
+import { time } from "console";
+import { format } from "date-fns";
 
 interface ServiceItemProps
 {
-  service: Service
+  barbershop: Barbershop;
+  service: Service;
   isAuthenticated?: boolean;
 
 }
 
 
-const ServiceItem  = ({service,isAuthenticated}: ServiceItemProps) => {
+const ServiceItem  = ({service,barbershop,isAuthenticated}: ServiceItemProps) => {
+  const [date, setDate] = useState<Date | undefined>(undefined);
+  const [hour, setHour] = useState<string | undefined>();
+
+  const handledaleClick = (date: Date | undefined) =>
+  {
+       setDate(date);
+       setHour(undefined)
+
+  }
+
+  const handleHourClick = (time:string) =>
+  {
+
+    setHour(time)
+  }
+  
+
  
  const handleBookingClick = () =>
  {
@@ -23,6 +50,27 @@ const ServiceItem  = ({service,isAuthenticated}: ServiceItemProps) => {
  return signIn("google")
   }
  }
+
+// Função para verificar se a data é sexta-feira ou sábado
+const isFridayOrSaturday = (date: Date) => {
+  const day = date.getDay();
+  return day === 5 || day === 6; // sexta-feira = 5, sábado = 6
+};
+  // Função para desabilitar todas as datas, exceto sexta-feira ou sábado
+  const tileDisabled = (date: Date) => {
+    return !isFridayOrSaturday(date);
+  };
+
+  const timeList  = useMemo( () => {
+    return date ? generateDayTimeList(date) : []
+
+  },[date])
+   
+  
+
+
+
+ 
     return ( 
 
         <Card>
@@ -45,8 +93,143 @@ const ServiceItem  = ({service,isAuthenticated}: ServiceItemProps) => {
                        }).format(Number(service.price))}
                         </p>
                                                                 
-                     
+                     <Sheet>
+                      <SheetTrigger asChild>
              <Button variant="secondary" onClick={handleBookingClick}>Reservar</Button>
+             </SheetTrigger>
+             <SheetContent className="p-0">
+              <SheetHeader className="text-left px-5 py-6 border-b border-solid border-secondary">
+                <SheetTitle>Fazer Reserva</SheetTitle>
+              </SheetHeader>
+              <div className="py-6 ">
+
+              <Calendar  mode="single" 
+              selected={date}   
+               onSelect={handledaleClick} 
+               locale={ptBR} 
+               fromDate={new Date()}
+               disabled ={tileDisabled}
+              
+
+               
+              
+              styles={{
+
+                head_cell:
+                {
+                     width: "100%",
+                     textTransform: "capitalize",
+
+
+                },
+                cell:{
+
+                  width: "100%",
+                },
+
+                button:
+                {
+                     width: "100%",
+
+
+                },
+                nav_button_previous:
+                {
+                     width: "32px",
+
+
+                },
+                nav_button_next:
+                {
+                     width: "32px",
+
+
+                },
+                caption:
+                {
+                 textTransform: "capitalize",
+
+                },
+                
+                
+              }}
+              
+              />
+              </div>
+             {date && (
+   
+            <div className=" flex gap-3 overflow-auto py-6 px-5 border-t border-solid border-secondary [&::-webkit-scrollbar]:hidden">
+            {timeList.map((time) => (
+              <Button
+               onClick={() =>handleHourClick(time)} 
+               variant={ hour === time ? 'default': "outline"}
+               className="rounded-full" 
+               key={time}
+               > 
+               {time} 
+               </Button>
+              
+              
+             
+                
+              
+            
+             ))}
+             </div>
+
+            
+              ) }
+               <div className="py-6 px-5 border-t border-solid border-secondary">
+               <Card>
+                <CardContent className="p-3 gap-3 flex flex-col">
+                      <div className="flex justify-between">
+                         <h2 className="font-bold">{service.name}</h2>
+                         <h3 className="font-bold text-sm">   
+                         {" "}
+                         {Intl.NumberFormat("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                                            
+                       }).format(Number(service.price))}
+                       </h3>
+                      </div>
+                      {date &&(
+                   <div className="flex justify-between">
+                    <h3 className="text-gray-400 text-sm">Data</h3>
+                    <h4 className="text-sm ">{format(date,"dd 'de' MMMM",{
+                      locale: ptBR,
+                    
+                     } )}</h4>
+                   </div>
+                      )}
+                          {hour &&(
+                   <div className="flex justify-between">
+                    <h3 className="text-gray-400 text-sm">Horário</h3>
+                    <h4 className="text-sm ">{hour}</h4>
+                    </div>
+                     )}
+
+                    {hour &&(
+                   <div className="flex justify-between">
+                    <h3 className="text-gray-400 text-sm">Barbearia</h3>
+                    <h4 className="text-sm ">{barbershop.name}</h4>
+                    </div>
+                     )}
+             
+                   
+                </CardContent>
+               </Card>
+               </div>
+
+               <SheetFooter className="px-5">
+               <Button disabled={!hour || !date}>
+                 Confirmar reserva
+               </Button>
+               </SheetFooter>
+                
+             
+             </SheetContent>
+             </Sheet>
                     
                     </div>
                   </div>
@@ -56,6 +239,7 @@ const ServiceItem  = ({service,isAuthenticated}: ServiceItemProps) => {
 
 
         </Card>
+        
 
      );
 }
